7eea7f2299be248f2a41d09776587fb2fe172c93
devholic <devholic@plusquare.com>
2015-10-10 01:50:15 +0900

    add native function 'getArchitecture'

--- test/Atosl.java
+++ test/Atosl.java
@@ -1,9 +1,11 @@
 public class Atosl {
-native int symbolicate(String arch, String executable, String[] addresses, int addressLength);
+native String getArchitecture(String dSYM);
+native int symbolicate(String arch, String dSYM, String[] addresses, int addressLength);
 static {
         System.load("atosl_library_path");
 }
 static public void main(String argv[]) {
+        //self	ViewController *	0x1275442a0	0x00000001275442a0
         String test1_Address[] = {"0x0000b1e7"};
         String test2_Address[] = {"0x00006ed7"};
         Atosl atosl = new Atosl();
