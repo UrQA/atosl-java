a0aebd53fdb38f635e24be589773a2e681a54fbb
devholic <devholic@plusquare.com>
2015-10-13 21:18:01 +0900

    update Atosl.java (test)

--- Atosl.java
+++ Atosl.java
@@ -4,9 +4,10 @@ static {
         System.load("/Users/devholic/Desktop/git/honeyqa/atosl-java/libatosl.so");
 }
 static public void main(String argv[]) {
-        //self	ViewController *	0x1275442a0	0x00000001275442a0
-        String test1_Address[] = {"0x1275442a0","0x00000001275442a0"};
+        // self	ViewController *	0x7fcd88495d40	0x00007fcd88495d40
+        // sender	UIButton *	0x7fcd8a001d20	0x00007fcd8a001d20
+        String test1_Address[] = {"0xcf000","0x0017ddd5"};
         Atosl atosl = new Atosl();
-        System.out.println(atosl.symbolicate("arm64", "./test/res/test.app.dSYM/Contents/Resources/DWARF/test", test1_Address, test1_Address.length));
-}
+        System.out.println(atosl.symbolicate("armv7", "./test/res/MapOut.app.dSYM/Contents/Resources/DWARF/MapOut", test1_Address, test1_Address.length));
+        }
 }
