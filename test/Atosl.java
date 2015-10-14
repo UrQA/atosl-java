868de0cd1393a32e4bd0179088641ee44deb3308
devholic <devholic@plusquare.com>
2015-10-14 17:11:39 +0900

    update .java / remove class file

--- test/Atosl.java
+++ test/Atosl.java
@@ -1,15 +1,13 @@
 public class Atosl {
-native String getArchitecture(String dSYM);
 native int symbolicate(String arch, String dSYM, String[] addresses, int addressLength);
 static {
         System.load("atosl_library_path");
 }
 static public void main(String argv[]) {
-        //self	ViewController *	0x1275442a0	0x00000001275442a0
-        String test1_Address[] = {"0x0000b1e7"};
-        String test2_Address[] = {"0x00006ed7"};
+        String test1_Address[] = {"0xcf000","0x0017ddd5"};
         Atosl atosl = new Atosl();
-        atosl.symbolicate("armv7s", "./res/CrashTest3Dwarf.thin", test1_Address, test1_Address.length);
-        atosl.symbolicate("armv7s", "./res/CrashTest3Dwarf.fat", test2_Address, test2_Address.length);
+        System.out.println(atosl.symbolicate("armv7",
+        "./test/res/MapOut.app.dSYM/Contents/Resources/DWARF/MapOut",
+        test1_Address, test1_Address.length));
 }
 }
