8f7b9ff5678c52d1fc1a7a03142ae98cc3fc17d5
Sunghoon Kang <devholic@plusquare.com>
2015-10-16 06:53:25 +0900

    remove path

--- test/Atosl.java
+++ test/Atosl.java
@@ -1,8 +1,7 @@
 public class Atosl {
 native String[] symbolicate(String arch, String dSYM, String[] addresses, int addressLength);
 static {
-        // System.load("atosl_library_path");
-        System.load("/Users/devholic/Desktop/git/honeyqa/atosl-java/test/libatosl.so");
+        System.load("atosl_library_path");
 }
 static public void main(String argv[]) {
         String test1_Address[] = {"0xcf000","0x0017ddd5"};
