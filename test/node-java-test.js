f0058f16a3d72400c8d96861d5764c1e5512df0a
devholic <devholic@plusquare.com>
2015-11-22 21:44:58 +0900

    Update node-java test

--- test/node-java-test.js
+++ test/node-java-test.js
@@ -3,5 +3,8 @@ java.classpath.push("./src");
 
 var Atosl = java.newInstanceSync("io.honeyqa.atosl.Atosl");
 
-var testArch = Atosl.getArchSync("/Users/devholic/Desktop/git/honeyqa/atosl-java/test/res/m2048.app.dSYM/Contents/Resources/DWARF/m2048");
-console.log(testArch.sizeSync());
+var testArch = Atosl.getArchSync("./res/m2048.app.dSYM/Contents/Resources/DWARF/m2048");
+
+for (var i = 0; i < testArch.sizeSync(); i++) {
+    console.log(testArch.getSync(i));
+}
