35f4197ee2a4267566cb5b62b4dc1de24b6ceb2a
devholic <devholic@plusquare.com>
2015-11-24 04:59:20 +0900

    Update node-java usage

--- test/node-java-test.js
+++ test/node-java-test.js
@@ -6,5 +6,5 @@ var Atosl = java.newInstanceSync("io.honeyqa.atosl.Atosl");
 var testArch = Atosl.getArchSync("./res/m2048.app.dSYM/Contents/Resources/DWARF/m2048");
 
 for (var i = 0; i < testArch.sizeSync(); i++) {
-    console.log(testArch.getSync(i));
+    console.log(Atosl.getUUIDSync(testArch.getSync(i), "./res/m2048.app.dSYM/Contents/Resources/DWARF/m2048"));
 }
